8a13d507a874d52eed1d5a1f06d9458ccdc4038a
Danny Hua <zaapen@gmail.com>
2022-06-07 20:27:41 -0700

    added favicon and process.env

--- data/database.js
+++ data/database.js
@@ -2,10 +2,16 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let mongodbUrl = 'mongodb://localhost:27017';
+
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+}
+
 let database;
 
 const connectToDatabase = async () => {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
+  const client = await MongoClient.connect(mongodbUrl);
   database = client.db('car-setup');
 };
 
